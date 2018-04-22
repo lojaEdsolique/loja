e725da6c372e4f66dc619cba55e62bd1bcb7d3ad
Edvaldo Solique <edsolique@gmail.com>
2018-04-22 10:08:05 -0400

    criei a classe page

--- index.php
+++ index.php
@@ -2,16 +2,17 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \EDS\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new EDS\DB\sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
-
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
