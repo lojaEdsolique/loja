753fd387213090380ab677cf23fac1ff2251cbb6
Edvaldo Solique <edsolique@gmail.com>
2018-04-22 19:31:06 -0400

    criei a pageadmin

--- index.php
+++ index.php
@@ -2,20 +2,28 @@
 
 require_once("vendor/autoload.php");
 
-use \Slim\Slim;
-use \EDS\Page;
+	use \Slim\Slim;
+	use \EDS\Page;
+	use \EDS\PageAdmin;
 
-$app = new Slim();
+	$app = new Slim();
 
-$app->config('debug', true);
+	$app->config('debug', true);
 
-$app->get('/', function() {
-    
-	$page = new Page();
-	$page->setTpl("index");
+	$app->get('/', function() {
+	    
+		$page = new Page();
+		$page->setTpl("index");
 
-});
+	});
 
-$app->run();
+	$app->get('/admin', function() {
+	    
+		$page = new PageAdmin();
+		$page->setTpl("index");
+
+	});
+
+	$app->run();
 
  ?>
---
+++ vendor/edsolique/php-classes/src/pageAdmin.php
@@ -0,0 +1,11 @@
+<?php 
+
+	namespace EDS;
+	class PageAdmin extends Page {
+		public function __construct($opts = array(), $tpl_dir = "/views/admin/")
+		{
+			parent::__construct($opts, $tpl_dir);
+		}
+	}
+
+ ?>
